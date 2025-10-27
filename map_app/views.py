782f2535251c6e8bcba4c57f4b3b20cb8de0fa66
samurai1221 <gorislavecz86@mail.ru>
2025-10-27 16:26:47 +0500

    1234

--- map_app/views.py
+++ map_app/views.py
@@ -2,6 +2,6 @@ from django.conf import settings
 from django.shortcuts import render
 
 def map_view(request):
-    return render(request, 'templates/map_app/index.html', {
+    return render(request, 'map_app/index.html', {
         'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY
     })
--- map_project/settings.py
+++ map_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ph1*#b-u(3lx+=_exl^2oz5ht$smv8-4hw$t7kxbj(xxa-utcb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
